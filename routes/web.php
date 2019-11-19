c7373a8dcb029e7b8ca55386ccb7f6b56e631086
dimitry <dimitry01@gmail.com>
2019-11-19 20:59:09 +0100

    adding_url

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 //Route::get('/user', 'CampaignsController@create');
 
 Route::get('/retry_jobs', function() {
-    Artisan::call('queue:retry all');
+    Artisan::call('queue:retry');
     return "Failed jobs restarted";
 });
 
