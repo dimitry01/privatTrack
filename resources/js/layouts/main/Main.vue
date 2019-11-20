ea92e50c4b492882a2e897e77066fc9b7e84925f
dimitry <dimitry01@gmail.com>
2019-11-20 10:31:08 +0100

    route

--- resources/js/layouts/main/Main.vue
+++ resources/js/layouts/main/Main.vue
@@ -70,8 +70,6 @@ export default {
 	},
 	watch: {
 		'$route'() {
-			console.log(this.routeTitle)
-			console.log(this.$route.meta.pageTitle)
 			this.routeTitle = this.$route.meta.pageTitle;
 		},
 		isThemeDark(val) {
@@ -155,6 +153,8 @@ export default {
 	},
 	created() {
 		this.setSidebarWidth();
+					console.log(this.routeTitle)
+			console.log(this.$route.meta.pageTitle)
 		if(this.navbarColor == "#fff" && this.isThemeDark) {
 			this.updateNavbarColor("#10163a")
 		}else {
